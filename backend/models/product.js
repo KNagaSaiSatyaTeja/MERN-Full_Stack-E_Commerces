fa9da16a43cf461b0a826d485b73406d26788f2c
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-04-07 23:17:38 +0530

    modified backend with rating and cors

--- backend/models/product.js
+++ backend/models/product.js
@@ -22,6 +22,10 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    rating: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,28 @@ import dotenv from "dotenv";
 import Users from "./routes/user.route.js";
 import Product from "./routes/porducts.route.js";
 import Cart from "./routes/cart.route.js";
-import multer from "multer";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3000",
+      "http://192.168.1.5:3001",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
-app.use("/api/user", Users);
-app.use("/api/product", Product);
+app.use("/api/users", Users);
+app.use("/api/products", Product);
 app.use("/api/cart", Cart);
 
 mongoose
@@ -21,8 +34,8 @@ mongoose
     console.log("connected successfully");
   })
   .then(
-    app.listen(process.env.PORT, () => {
-      console.log("conneted to port 3000");
+    app.listen(process.env.PORT || 5001, () => {
+      console.log("conneted to port");
     })
   )
   .catch((err) => {
