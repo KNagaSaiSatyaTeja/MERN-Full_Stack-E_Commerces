c287396b832fe6f2273d502ad84df2f3bd2e54e5
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-07-23 23:14:14 +0530

    modified the logging and response message

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(
       "http://localhost:3001",
       "http://localhost:3000",
       "http://localhost:5000",
-      "http://192.168.1.5:3001","*"
+      "http://192.168.1.5:3001",
+      "*",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -30,15 +31,13 @@ app.use("/api/products", Product);
 app.use("/api/cart", Cart);
 
 mongoose
-  .connect(process.env.MONGO_URl)
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("connected successfully");
   })
-  .then(
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("conneted to port", process.env.PORT || 5000);
-    })
-  )
   .catch((err) => {
     console.log("error while connect ", err.message);
   });
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
